89d92c23a38f1e30d3affd9e480140da23b9e8ca
Shane <43347614+andersonshane@users.noreply.github.com>
2019-06-04 14:12:52 +0100

    EditEntry form

--- DBSManagmentSystem/EditEntry.cs
+++ DBSManagmentSystem/EditEntry.cs
@@ -16,5 +16,20 @@ namespace DBSManagmentSystem
         {
             InitializeComponent();
         }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
